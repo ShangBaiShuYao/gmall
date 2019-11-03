cd680c0c2f5a10a06d88d596fda8751e77f48ab2
上白书妖 <shangbaishuyao@163.com>
2019-11-03 14:42:47 +0800

    查SPU下的SKU

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/controller/SkuInfoController.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/controller/SkuInfoController.java
@@ -1,12 +1,14 @@
 package com.atguigu.gmall.pms.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 
 import com.atguigu.core.bean.PageVo;
 import com.atguigu.core.bean.QueryCondition;
 import com.atguigu.core.bean.Resp;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +34,22 @@ import com.atguigu.gmall.pms.service.SkuInfoService;
 public class SkuInfoController {
     @Autowired
     private SkuInfoService skuInfoService;
+
+
+
+    /*
+     * @Description   查询SPU下的SKU
+     * @Date   2019/11/3 14:36
+     */
+    @ApiOperation("查询spu下的sku")
+    @GetMapping("{spuId}")
+    public Resp<List<SkuInfoEntity>> querySkuBySpuId(@PathVariable("spuId")Long spuId){
+
+        List<SkuInfoEntity> skuInfoEntities = this.skuInfoService.list(new QueryWrapper<SkuInfoEntity>().eq("spu_id", spuId));
+
+        return Resp.ok(skuInfoEntities);
+    }
+
 
     /**
      * 列表
